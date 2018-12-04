216f23df9afa9f19265ce60ee7807dc33efd5de9
Wesley Donker <DonkerNET@users.noreply.github.com>
2018-12-04 10:55:29 +0100

    Updated 'Get database sizes.sql' script to calculate the total size in MB

--- T-SQL/Get database sizes.sql
+++ T-SQL/Get database sizes.sql
@@ -4,10 +4,12 @@ as
     select database_id, type, size * 8.0 / 1024 size
     from sys.master_files
 )
-select *
+select
+	*,
+	(dt.DataFileSizeMB + dt.LogFileSizeMB) AS TotalSizeMB
 from (
 	select 
-		name,
+		[name] AS [Name],
 		(select sum(size) from fs where type = 0 and fs.database_id = db.database_id) DataFileSizeMB,
 		(select sum(size) from fs where type = 1 and fs.database_id = db.database_id) LogFileSizeMB
 	from sys.databases db
